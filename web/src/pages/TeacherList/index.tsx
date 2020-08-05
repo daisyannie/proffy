bd8d1b03cce296118db72b615b8a9712f9ea6d2d
Daisy Annie <daisy.annie@shift.com.br>
2020-08-05 15:39:31 -0300

    Commit do projeto desenvolvido no terceiro dia do NLW 2020.

---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,92 @@
+import React, { useState, FormEvent } from 'react';
+
+import PageHeader from '../../components/PageHeader';
+import TeacherItem, { Teacher } from '../../components/TeacherItem';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
+import api from '../../services/api';
+
+import './styles.css';
+
+function TeacherList(){
+
+    const [teachers, setTeachers] = useState([]);
+
+    const [subject, setSubject] = useState('');
+    const [week_day, setWeekDay] = useState('');
+    const [time, setTime] = useState('');
+
+    async function searchTeachers(e: FormEvent){
+        e.preventDefault();
+
+        // POST ou PUT pode mandar os parâmetros direto. Mas do tipo GET tem que mandar como params
+        const response = await api.get('classes',{
+            params: {
+                subject,
+                week_day,
+                time
+            }
+        });
+
+        setTeachers(response.data);
+    }
+
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader title="Estes são os proffys disponíveis.">
+                <form id="search-teachers" onSubmit={searchTeachers}>                
+                <Select 
+                        name="subject"
+                        label="Matéria" 
+                        value={subject}
+                        onChange={(event) =>{ setSubject(event.target.value) }}
+                        options={[                            
+                            { value: 'Português', label: 'Português' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Geografia', label: 'Geografia' },
+                            { value: 'Ciências', label: 'Ciências' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Biologia', label: 'Biologia' },
+                        ]}
+                    />
+                    <Select 
+                        name="week_day"
+                        label="Dia da semana" 
+                        value={week_day}
+                        onChange={(event) =>{ setWeekDay(event.target.value) }}
+                        options={[                            
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
+                    <Input 
+                        name="time" 
+                        label="Hora" 
+                        type="time" 
+                        value={time}
+                        onChange={(event) =>{ setTime(event.target.value) }}
+                    />
+                    <button type="submit">
+                        Buscar
+                    </button>
+                </form>
+            </PageHeader>
+
+            <main>
+                {teachers.map((teacher: Teacher) => {
+                    return <TeacherItem key={teacher.id} teacher={teacher} />
+                })}
+            </main>
+        </div>
+    )
+}
+
+export default TeacherList;
